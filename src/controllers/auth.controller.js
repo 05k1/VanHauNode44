1299e8b547cf8ce8eb442e4dea8b451d7cc27080
Tran Hau <hautran.01022002@gmail.com>
2024-09-28 17:30:15 +0700

    API LOGIN

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import sequelize from "../models/connect.js";
 import initModels from "../models/init-models.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const model = initModels(sequelize);
 
@@ -30,7 +32,7 @@ const register = async (req, res, next) => {
     const userNew = await model.users.create({
       full_name: fullName,
       email: email,
-      pass_word: pass,
+      pass_word: bcrypt.hashSync(pass, 10),
     });
 
     return res.status(200).json({
@@ -42,4 +44,57 @@ const register = async (req, res, next) => {
   }
 };
 
-export { register };
+const login = async (req, res) => {
+  try {
+    // B1: lay eamil va pass_word tu body req
+
+    // B2: check user thong qua email (get user tu db)
+
+    // Neu khong co user => ra erorr user not found
+
+    // Neu co user => check pass_word
+
+    //    Neu pass_word ko trung nhau => pass_word is wrong
+
+    //   Neu pass_word trung nhau => tao access token
+
+    let { email, pass_word } = req.body;
+    let user = await model.users.findOne({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return res.status(400).json({ message: "Email is wrong" });
+    }
+
+    let checkPass = bcrypt.compareSync(pass_word, user.pass_word);
+
+    if (!checkPass) {
+      return res.status(400).json({ message: "password is wrong" });
+    }
+    let payload = {
+      userId: user.user_id,
+    };
+    // access token, refresh token
+    // Tao token
+    // Func sign jsonwebtoken
+    // param 1: tao payload va luu vao token
+    // param 2: key de tao token
+    // param 3: setting lifetime cua token va thuat toan de tao token
+    let accessToken = jwt.sign({ payload }, "NODE44", {
+      algorithm: "HS256",
+      expiresIn: "1d",
+    });
+    res.status(200).json({
+      message: "login successfully",
+      data: {
+        token: accessToken,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "error" });
+  }
+};
+
+export { register, login };
