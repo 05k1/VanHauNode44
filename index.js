7a82795fa5f2e40a298d4b98e90977f90d868dd2
Tran Hau <hautran.01022002@gmail.com>
2024-10-26 14:30:53 +0700

    #16

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import express from "express";
 import rootRoutes from "./src/routes/root.router.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 // tao doi tuong express
 const app = express();
@@ -18,6 +22,59 @@ app.use(
     credentials: true, // cho phep fe lay cookie va luu vao cookie
   })
 );
+
+// tao http server
+const server = createServer(app);
+
+// tao socketIO server
+// io: obj cua socket server
+// socket: obj cua socket client
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// lang nghe event ket noi tu client (FE) qua socketIO
+// on nhan event
+// emit gui event
+// co 2 params
+// params1 event type
+// param2 function
+
+let number = 0;
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("send-click", () => {
+    console.log("FE send click");
+    number += 1;
+    // server ban event cho tat ca client
+    io.emit("send-new-number", number);
+  });
+  socket.on("reduce-number", () => {
+    number -= 1;
+    // server ban event cho tat ca client
+    io.emit("send-new-number", number);
+  });
+
+  // nhan event send message
+  socket.on("send-mess", async ({ user_id, content }) => {
+    let newChat = {
+      user_id,
+      content,
+      date: new Date(),
+    };
+    // luu chat vao db
+    await prisma.chat.create({
+      data: newChat,
+    });
+    // server ban event cho tat ca client
+    io.emit("sv-send-mess", { user_id, content });
+  });
+});
+
+// BE nhan event tu fe client gui ve
 
 // them middleware de doc data json
 app.use(express.json());
@@ -54,6 +111,6 @@ app.get("/test-header", (req, res) => {
   res.send(headers);
 });
 
-app.listen(8080, () => {
+server.listen(8080, () => {
   console.log("Server running on port 8080");
 });
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -41,21 +41,21 @@ const getUsers = async (req, res) => {
           [Op.like]: `%${full_name}%`,
         },
       },
-      attributes: ["full_name"],
-      include: [
-        {
-          model: model.video, // chon model muon ket ban
-          as: "videos",
-          attributes: ["video_name", "user_id"], // chi dinh truong nao se hien hti
-          required: true, // default se ket bang left join, muon inner join required: true
-          include: [
-            {
-              model: model.video_comment,
-              as: "video_comments",
-            },
-          ],
-        },
-      ],
+      // attributes: ["full_name"],
+      // include: [
+      //   {
+      //     model: model.video, // chon model muon ket ban
+      //     as: "videos",
+      //     attributes: ["video_name", "user_id"], // chi dinh truong nao se hien hti
+      //     required: true, // default se ket bang left join, muon inner join required: true
+      //     include: [
+      //       {
+      //         model: model.video_comment,
+      //         as: "video_comments",
+      //       },
+      //     ],
+      //   },
+      // ],
     });
     return res.status(OK).json(data);
   } catch (error) {
